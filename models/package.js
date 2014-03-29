623823cbc3fe6e8bb47d2a34298f1946e47517d0
Todd Wolfson <todd@twolfson.com>
2014-03-29 06:03:52 -0700

    Repaired stripping of query strings from image URLs
    Rolling back most changes
    
    Moved from query to search and everything is right in the world =D
    
    After much debugging, I found the issue back where we started but got trolled out of looking for some reason -_-;;
    
    Remove dev statements
    
    Even simpler
    
    Simplifying code to reduce confusion
    
    Still jumping around trying to find the point of introduction
    
    Thought we found the issue but we didn't =(
    
    Resetting all logs
    
    Stepping through a lot of actions
    
    Discovered our input URL dos not receive a query string
    
    Moving on to README images
    
    Console.error works as well, woot. Let's go diving!
    
    Finally made progress due to error levels
    
    Attempting to get information to output to the damn screen
    
    node_module updates

--- models/package.js
+++ models/package.js
@@ -22,7 +22,7 @@ function urlPolicy (pkgData) {
         protocol: v.protocol,
         host: v.host,
         pathname: v.pathname,
-        query: u.query_,
+        search: u.query_,
         hash: u.fragment_
       }
     } else {
@@ -30,7 +30,7 @@ function urlPolicy (pkgData) {
         protocol: u.scheme_ + ':',
         host: u.domain_ + (u.port_ ? ':' + u.port_ : ''),
         pathname: u.path_,
-        query: u.query_,
+        search: u.query_,
         hash: u.fragment_
       }
     }
