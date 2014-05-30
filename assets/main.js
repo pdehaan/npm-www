8a9cefb07eaaf80003bbccf90244ccdff6f7fa11
bcoe <bencoe@gmail.com>
2014-05-30 14:41:18 -0400

    made package url relative.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 require('npm-typeahead')({
-  npmUrl: 'https://www.npmjs.org',// URL to re-direct the user to.
+  npmUrl: '',// URL to re-direct the user to.
   searchUrl: 'http://typeahead-1.aws-west.internal.npmjs.com', // URL for search npm-typeahead REST server.
   $: $ // jQuery dependency.
 });
