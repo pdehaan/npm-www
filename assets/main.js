b5744df599a3ca2caac8a2a362fbec993d46342d
bcoe <bencoe@gmail.com>
2014-05-30 14:09:27 -0400

    fixed typo in npm url.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 require('npm-typeahead')({
-  npmUrl: 'https://www.nmpjs.org',// URL to re-direct the user to.
+  npmUrl: 'https://www.npmjs.org',// URL to re-direct the user to.
   searchUrl: 'http://typeahead-1.aws-west.internal.npmjs.com', // URL for search npm-typeahead REST server.
   $: $ // jQuery dependency.
 });
