0462222f2484ff47af9a858260340f1237451776
C J Silverio <ceej@npmjs.com>
2014-06-20 13:12:28 -0700

    Remove zag-agent from www dependencies. Replace it with a numbat emitter. Wrap the emitter in a facade that imitates the zag API so no metrics calls need to change.
    Remove the unused installation of bluebird.

--- metrics-client.js
+++ metrics-client.js
@@ -1,4 +1,5 @@
-var ZagAgent = require('zag-agent')
+var Emitter = require('numbat-emitter')
+  , os = require('os')
   , metrics
 
 module.exports = function Metrics () {
@@ -10,7 +11,8 @@ module.exports = function Metrics () {
   var config = require("./config.js")
   // define the metrics agent
   if (config && config.metrics) {
-    metrics = ZagAgent(config.metrics.collectors).scope(config.metrics.prefix)
+    metrics =
+    metrics = new EmitterFacade(config.metrics.collectors, config.metrics.prefix)
   } else {
     metrics = { histogram: function() {}, counter: function() {}, close: function() {} }
   }
@@ -18,4 +20,32 @@ module.exports = function Metrics () {
   return metrics
 }
 
+/*
+  Since the old web site has a limited lifespan, I'm minimizing disturbance to it
+  when I swap out the metrics client. This is a facade that presents the zag agent
+  API so none of the callers need to change.
+*/
 
+function EmitterFacade(collector, prefix) {
+  this.client = Emitter({
+    host: collector.host,
+    port: collector.port,
+    node: os.hostname()
+  })
+  this.prefix = prefix || 'npm-www'
+}
+
+EmitterFacade.prototype.histogram = function(name, value) {
+  value = value || 1
+  this.client.metric({ name: name, value: value, tags: ['histogram']})
+}
+
+EmitterFacade.prototype.counter = function(name, count) {
+  count = count || 1
+  this.client.metric({ name: name, count: count, tags: ['counter']})
+}
+
+EmitterFacade.prototype.close = function() {
+  this.client.destroy();
+  this.client = null;
+}
