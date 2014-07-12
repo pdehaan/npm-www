2d13bd883f481cce90e5057839fa44a303f3d11c
rockbot <raquel@rckbt.me>
2014-07-11 17:54:17 -0700

    adds link to homepage plus GA event tracking

--- routes/npme-beta.js
+++ routes/npme-beta.js
@@ -6,9 +6,11 @@ td.title = "npm-Enterprise beta"
 
 function npme (req, res) {
   req.model.load("profile", req)
+  req.model.load("whoshiring")
   req.model.end(function(er, m) {
     if(er) return res.error(er)
     td.profile = m.profile
+    td.hiring = m.whoshiring
 
     if (req.pathname.indexOf('thanks') !== -1) {
       return res.template('npme-beta-thanks.ejs', td)
